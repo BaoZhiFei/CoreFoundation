96516b87ef580a1a8b694de07593ecb07ee3722a
baozhifei <baozhifei@gmail.com>
2021-10-18 20:50:40 +0800

    add project

---
+++ CoreFoundation/CoreFoundation.h
@@ -0,0 +1,18 @@
+//
+//  CoreFoundation.h
+//  CoreFoundation
+//
+//  Created by mac on 2021/10/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreFoundation.
+FOUNDATION_EXPORT double CoreFoundationVersionNumber;
+
+//! Project version string for CoreFoundation.
+FOUNDATION_EXPORT const unsigned char CoreFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreFoundation/PublicHeader.h>
+
+
